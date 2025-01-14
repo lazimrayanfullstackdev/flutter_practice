4035e25b1ba73612aa35f1d7ece7b22fdac61650
Rayan <lazimrayanfreelancer@gmail.com>
2025-01-14 17:59:15 +0600

    Flutter overflow, safearea

--- hello/lib/main.dart
+++ hello/lib/main.dart
@@ -2,19 +2,27 @@ import 'package:flutter/material.dart';
 
 void main(){
  runApp(MaterialApp(
-   home: Scaffold(
+   title: "Rayan's App",
+   home: SafeArea(child: Scaffold(
      appBar: AppBar(
        backgroundColor: Colors.deepPurpleAccent,
-       title: Text("Hello"),
+       title: Text("Hello",
+         style: TextStyle(
+           fontSize: 30,
+           color: Colors.white,
+         ),),
        centerTitle: true, //to center the title
      ),
      body: Center(child: Text(
          "Hello World",
        style: TextStyle(
-         fontSize: 25,
+         fontSize: 50,
+         color: Colors.deepPurple,
+         fontWeight: FontWeight.w300,
+         overflow: TextOverflow.ellipsis,
        ),
          textDirection: TextDirection.ltr,
      )),
    ),
- ));
+ )));
 }
