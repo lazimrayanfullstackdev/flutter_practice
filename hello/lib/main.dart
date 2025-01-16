9f9202afffd44c8bc442d3cc3a258e1a37978b28
Rayan <lazimrayanfreelancer@gmail.com>
2025-01-16 12:03:33 +0600

    Column, Row and alignment

--- hello/lib/main.dart
+++ hello/lib/main.dart
@@ -13,16 +13,195 @@ void main(){
          ),),
        centerTitle: true, //to center the title
      ),
-     body: Center(child: Text(
-         "Hello World",
-       style: TextStyle(
-         fontSize: 50,
-         color: Colors.deepPurple,
-         fontWeight: FontWeight.w300,
-         overflow: TextOverflow.ellipsis,
-       ),
-         textDirection: TextDirection.ltr,
-     )),
+     body: SingleChildScrollView(
+       child: Column(
+       mainAxisAlignment: MainAxisAlignment.center,
+       children: [
+         Row(
+           children: [
+             Text("Text 1",style: TextStyle(
+               fontSize: 25,
+               color: Colors.black,
+             ),),
+             Text("Text 2",style: TextStyle(
+               fontSize: 25,
+               color: Colors.black,
+             ),),
+             Text("Text 3",style: TextStyle(
+               fontSize: 25,
+               color: Colors.black,
+             ),),
+           ],
+         ),
+         Text("Text 5",style: TextStyle(
+       fontSize: 25,
+       color: Colors.black,
+     ),),
+         Text("Text 2",style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 3",style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 4", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 5", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 6", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 7", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 8", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 9", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 10", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 11", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 12", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 13", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 14", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 15", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 16", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 17", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 18", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 19", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 20", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 21", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 22", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 23", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 24", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 25", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 26", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 27", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 28", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 29", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 30", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 31", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 32", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 33", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 34", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 35", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 36", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 37", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 38", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 39", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 40", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+         Text("Text 41", style: TextStyle(
+           fontSize: 25,
+           color: Colors.black,
+         ),),
+       ],
+     ),
    ),
- )));
+ )
+   )
+ )
+ );
 }
