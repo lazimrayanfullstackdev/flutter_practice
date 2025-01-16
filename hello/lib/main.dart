3c6479885ff9c9c480a2d1a80f587e40f6068be0
Rayan <lazimrayanfreelancer@gmail.com>
2025-01-16 17:51:29 +0600

    Container, boxshadow etc

--- hello/lib/main.dart
+++ hello/lib/main.dart
@@ -13,214 +13,53 @@ void main(){
          ),),
        centerTitle: true, //to center the title
      ),
-     body: SingleChildScrollView(
-       child: Column(
-       mainAxisAlignment: MainAxisAlignment.center,
-       children: [
-         SingleChildScrollView(
-           scrollDirection: Axis.horizontal,
-          child: Row(
-           children: [
-             Text("Text 1",style: TextStyle(
-               fontSize: 25,
-               color: Colors.black,
-             ),),
-             Text("Text 2",style: TextStyle(
-               fontSize: 25,
-               color: Colors.black,
-             ),),
-             Text("Text 3",style: TextStyle(
-               fontSize: 25,
-               color: Colors.black,
-             ),),
-             Text("Text 2",style: TextStyle(
-         fontSize: 25,
-         color: Colors.black,
-       ),),
-             Text("Text 3",style: TextStyle(
-         fontSize: 25,
-         color: Colors.black,
-       ),),
-             Text("Text 2",style: TextStyle(
-         fontSize: 25,
-         color: Colors.black,
-       ),),
-             Text("Text 3",style: TextStyle(
-         fontSize: 25,
-         color: Colors.black,
-       ),),
-       ],
+     body: Column(
+         children: [ Center(
+                     child: ElevatedButton(
+             style: ElevatedButton.styleFrom(
+               backgroundColor: Colors.green,
+               shape: RoundedRectangleBorder(
+                 borderRadius: BorderRadius.circular(25)
+               )
+             ),
+       onPressed: (){
+            print("This button is clicked");
+         }, child:
+           Text("Send",style: TextStyle(
+           color: Colors.white,
+           fontSize: 25
          ),
-   ),
-         Text("Text 5",style: TextStyle(
-       fontSize: 25,
-       color: Colors.black,
-     ),),
-         Text("Text 2",style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 3",style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 4", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 5", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 6", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 7", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 8", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 9", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 10", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 11", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 12", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 13", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 14", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 15", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 16", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 17", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 18", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 19", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 20", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 21", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 22", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 23", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 24", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 25", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 26", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 27", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 28", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 29", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 30", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 31", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 32", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 33", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 34", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 35", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 36", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 37", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 38", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 39", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 40", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         Text("Text 41", style: TextStyle(
-           fontSize: 25,
-           color: Colors.black,
-         ),),
-         ],
+                     )
+         )
      ),
+          Icon(Icons.ac_unit_outlined, size: 30, color: Colors.red),
+           TextButton(onPressed: (){
+             print("I am text button");
+           }, child: Text("Click Me", style: TextStyle(
+             color: Colors.deepPurpleAccent,fontSize: 20
+           ),)),
+           IconButton(onPressed: (){
+             print("I am icon button");
+           }, icon: Icon(Icons.add_a_photo, size: 30, color: Colors.black)),
+
+           InkWell(
+           onTap: (),
+               child: Container(
+             height: 200,
+             width: 200,
+             margin: EdgeInsets.only(top: 20),
+             padding: EdgeInsets.all(20),
+             alignment: Alignment.center,
+             color: Colors.amber,
+             child: Text("I am Rayan",style: TextStyle(
+               color: Colors.black,
+               fontSize: 25
+             ),),
+           )
+ ]
+ )
    ),
  )
-   )
  )
  );
 }
