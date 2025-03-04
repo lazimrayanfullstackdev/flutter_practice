54a1cc65f501918d8964b3cbbcb287584998f4bb
Rayan <lazimrayanfreelancer@gmail.com>
2025-03-04 16:25:56 +0600

    Product Update, Delete using API with model

--- todoapp/lib/ProductController.dart
+++ todoapp/lib/ProductController.dart
@@ -3,14 +3,17 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:todoapp/utils/urls.dart';
 
+import 'model/product.dart';
+
 class ProductController {
-  List products = [];
+  List<Data> products = [];
 
   Future<void> fetchProduct() async {
     final response = await http.get(Uri.parse(Urls.readProduct));
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
-      products = data['data'];
+      productModel model = productModel.fromJson(data);
+      products = model.data ?? [];
     }
   }
 
@@ -29,7 +32,36 @@ class ProductController {
           })
       );
       if (response.statusCode == 201) {
-        fetchProduct();
+        await fetchProduct();
       }
     }
+
+  Future<void> updateProduct(String id, String name, String img, String code, int qty, int price, int totalPrice) async {
+    final response = await http.post(Uri.parse(Urls.createProduct),
+        headers: {
+          'Content-Type': 'application/json'},
+        body: jsonEncode({
+          "ProductName": name,
+          "ProductCode": code,
+          "Img": img,
+          "Qty": qty,
+          "UnitPrice": price,
+          "TotalPrice": totalPrice
+        })
+    );
+    if (response.statusCode == 201) {
+      await fetchProduct();
+    }
+  }
+
+  Future<bool> deleteProduct(String id) async {
+    final response = await http.get(Uri.parse(Urls.deleteProduct(id)));
+    if (response.statusCode == 200) {
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+
   }
--- todoapp/lib/api_crud.dart
+++ todoapp/lib/api_crud.dart
@@ -12,7 +12,9 @@ class _ApiCrudState extends State<ApiCrud> {
 
   final ProductController productController = ProductController();
 
-  void productDialog(){
+  void productDialog({String? id, int? code, String? name, int? qty, String? img, int? unitPrice, int? totalPrice}){
+
+
     TextEditingController productNameController = TextEditingController();
     TextEditingController productCodeController = TextEditingController();
     TextEditingController productQtyController = TextEditingController();
@@ -20,8 +22,16 @@ class _ApiCrudState extends State<ApiCrud> {
     TextEditingController productUnitPriceController = TextEditingController();
     TextEditingController productTotalPriceController = TextEditingController();
 
+    productNameController.text = name ?? "";
+    productQtyController.text = qty.toString() ?? '';
+    productImageController.text = img ?? '';
+    productCodeController.text = code.toString() ?? '';
+    productUnitPriceController.text = unitPrice.toString() ?? '';
+    productTotalPriceController.text = totalPrice.toString() ?? '';
+
+
     showDialog(context:  context, builder: (context) =>AlertDialog(
-      title: Text("Add Product"),
+      title: Text(id == null ? "Add Product" : "Update Product"),
       content: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -66,13 +76,17 @@ class _ApiCrudState extends State<ApiCrud> {
             mainAxisSize: MainAxisSize.min,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              ElevatedButton(onPressed: (){
-                setState(() {
-                  productController.createProduct(productNameController.text, productImageController.text, productCodeController.text, int.parse(productQtyController.text), int.parse(productUnitPriceController.text), int.parse(productTotalPriceController.text));
+              ElevatedButton(onPressed: () {
+
+                  if(id== null){
+                    productController.createProduct(productNameController.text, productImageController.text, productCodeController.text, int.parse(productQtyController.text), int.parse(productUnitPriceController.text), int.parse(productTotalPriceController.text));
+                  }else{
+                    productController.updateProduct(id,productNameController.text, productImageController.text, productCodeController.text, int.parse(productQtyController.text), int.parse(productUnitPriceController.text), int.parse(productTotalPriceController.text));
+                  }
                   fetchData();
                   Navigator.pop(context);
-                });
-              }, child: Text("Add Product", style: TextStyle(color: Colors.white),), style: ElevatedButton.styleFrom(backgroundColor: Colors.deepPurple),),
+                  setState(() {});
+              }, child: Text(id == null ? "Add Product" : "Update Product", style: TextStyle(color: Colors.white),), style: ElevatedButton.styleFrom(backgroundColor: Colors.deepPurple),),
               SizedBox(width: 10,),
               TextButton(onPressed: (){
                 Navigator.pop(context);
@@ -112,17 +126,46 @@ class _ApiCrudState extends State<ApiCrud> {
               elevation: 4,
               margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
               child: ListTile(
-                leading: Image.network(product['Img'], width: 50,),
-                title: Text(product['ProductName'],style: TextStyle(fontWeight: FontWeight.bold),),
-                subtitle: Text('Price: \$${product['UnitPrice']}  | Qty: ${product['Qty']}', style: TextStyle(),),
+                leading: Image.network(product.img.toString(), width: 50,),
+                title: Text(product.productName.toString(),style: TextStyle(fontWeight: FontWeight.bold),),
+                subtitle: Text('Price: \$${product.unitPrice}  | Qty: ${product.qty}', style: TextStyle(),),
                 trailing: Row(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    IconButton(onPressed: ()=>productDialog(), icon: Icon(Icons.edit)),
+                    IconButton(onPressed: ()=>productDialog(
+                        id : product.sId,
+                        code: product.productCode,
+                        name: product.productName ,
+                        qty: product.qty,
+                        img: product.img,
+                        unitPrice: product.unitPrice,
+                        totalPrice: product.totalPrice,
+                    ), icon: Icon(Icons.edit)),
                     SizedBox(
                       width: 10,
                     ),
-                    IconButton(onPressed: (){}, icon: Icon(Icons.delete, color: Colors.red,))
+                    IconButton(onPressed: (){
+                      setState(() {
+                        productController.deleteProduct(product.sId.toString()).then((value){
+                          if(value){
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text("Product Deleted"),
+                                duration: Duration(seconds: 2),
+                              ),
+                            );
+                          }else{
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text("Something went wrong, try again"),
+                                duration: Duration(seconds: 2),
+                              ),
+                            );
+                          }
+                        });
+                      });
+
+                    }, icon: Icon(Icons.delete, color: Colors.red,))
                   ],
                 ),
               ),
